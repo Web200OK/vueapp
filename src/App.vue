e3c7f1d916e46e194199b5ce72271fdab89b20b6
myname <1691929357@qq.com>
2022-05-17 16:10:27 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="你好啊，这是自动化部署"/>
   </div>
 </template>
 
